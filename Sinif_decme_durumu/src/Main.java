8eb129c09e0d92cab6b28b422a1a5fef187bbcfd
hmgny <hmerveguney@hotmail.com>
2025-01-26 20:05:46 +0300

    Notlara 0-100 aralığında olma koşulu eklendi

--- Sinif_decme_durumu/src/Main.java
+++ Sinif_decme_durumu/src/Main.java
@@ -13,24 +13,35 @@ public class Main {
         System.out.print("Matematik notunuz: ");
         mat = input.nextInt();
 
+
         System.out.print("Fizik notunuzu giriniz: ");
         fizik = input.nextInt();
+
 
         System.out.print("Türkçe notunuzu giriniz: ");
         turkce = input.nextInt();
 
+
         System.out.print("Kimya notunuzu giriniz: ");
         kimya = input.nextInt();
+
 
         System.out.println("Müzik notunuzu giriniz: ");
         muzik = input.nextInt();
 
-        avarage = (mat + fizik + turkce + kimya + muzik)/5;
 
-        if(avarage > 55){
-            System.out.println("Ortalamanız: " + avarage + ". Sınıfı geçtiniz."  );
+        if(mat<0 || mat>100 || fizik<0 || fizik>100 || turkce<0 || turkce>100 || kimya<0 || kimya>100 || muzik<0 || muzik>100){
+            System.out.println("Hatalı bir değer girdiniz!");
         } else{
-            System.out.println("Ortalamanız: " + avarage + ". Sınıfta kaldınız. Daha çok çalışmalısınız!");
+            avarage = (mat + fizik + turkce + kimya + muzik)/5.0;
+            if(avarage > 55){
+                System.out.println("Ortalamanız: " + avarage + ". Sınıfı geçtiniz."  );
+            } else{
+                System.out.println("Ortalamanız: " + avarage + ". Sınıfta kaldınız. Daha çok çalışmalısınız!");
+            }
+
         }
+
+
     }
 }
